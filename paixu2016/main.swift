cc3359899baf568673a2fe63133276ca5992f516
wuchaopang <20141105049wch@mac23.local>
2016-06-21 10:56:31 +0800

    拍序2016
    通过对输入的20个数字进行按由小到达排序，用到的是for循环语句，通过冒泡排序进行结果的实现，for循环的使用是按照swift的格式进行使用，f
    or循环语句在编写时要注意格式问题，容易出现编译问题，实现框架for var j = 0;j<q.count-1-i;j++｛｝

--- paixu2016/main.swift
+++ paixu2016/main.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 print("Hello, World!")
-var q = [1,2,3,4,7,6,5,8,9,10,23,543,27,89,76,47,87,33,55,66]
+/*var q = [1,2,3,4,7,6,5,8,9,10,23,543,27,89,76,47,87,33,55,66]
 for var i = 0 ;i < q.count-1 ; i++
 {
     for var j = 0;j<q.count-1-i;j++
@@ -23,26 +23,25 @@ for var i = 0 ;i < q.count-1 ; i++
     }
 
 }
-print(q)
-/*func bubbleSort(){
+print(q)*/
 var q = [1,2,3,4,7,6,5,8,9,10,23,543,27,89,76,47,87,33,55,66]
 var x, y,z,e,m:Int
-for x in 0...q.count
+var q1:[Int]=[]
+for x in 0..<q.count
 {
   z=(q.count-1)-x
-    for y in 0...z
+    for y in 0..<z
     {
     e=q[y]
-     if(e > q[y+1])
+    if(e > q[y+1])
      {
         m=q[y+1]
-        q[y+1]=e
+       q[y+1]=e
         q[y]=m
     }
      
     }
-  
 }
-    print(q)
+  
+print(q)
 
-}*/
